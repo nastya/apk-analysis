2978321acb6ba7d058eb1144bfee45b4e72b78b3
Анастасия Сковорода <nastya_jane@mail.ru>
2015-10-19 12:14:15 +0300

    Scripts for building permission models

---
+++ build_models/build_perm_models.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import sys
+import operator
+import json
+sys.path.append('../')
+import system_perms
+import hashlib
+import os
+
+sys.path.append('../../androguard')
+from androguard.core.bytecode import *
+from androguard.core.bytecodes.apk import *
+from androguard.core.analysis.analysis import *
+import androlyze as anz
+
+fv_directory = '../perms_fv'
+if not os.path.exists(fv_directory):
+    os.makedirs(fv_directory)
+analyzed_apps = 'malware_for_models.txt'
+
+f = open(analyzed_apps, 'r')
+total_apps = sum(1 for line in open(analyzed_apps))
+
+count_apps = 0
+for line in f:
+	package_name = line[:-1]
+	count_apps += 1
+	print 'Processing', package_name, '(', count_apps, ' / ', total_apps, ')'
+	
+	#getting permissions
+	try:
+            a = APK(package_name)
+            perms = a.get_permissions()
+            perms_set = set(perms)
+        except:
+            print 'Failed to get manifest'
+            continue
+	
+	#building feature vector
+	perms_fv = {}
+	for perm in system_perms.permissions:
+            perms_fv[perm] = 0
+	for perm in perms_set:
+            if perm in perms_fv:
+                perms_fv[perm] = 1
+                
+        #saving
+        apk_hash = hashlib.sha256(open(package_name, 'r').read()).hexdigest()
+        out_f = open(fv_directory + '/' + apk_hash, 'w')
+        out_f.write(json.dumps(perms_fv, sort_keys=True, indent=4, separators=(',', ': ')))
+        out_f.close()
+        
+        
+f.close()
+
