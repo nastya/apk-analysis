d205948d0cdf9bb6d64000bbbaec9c0fb5b03f1d
Анастасия Сковорода <nastya_jane@mail.ru>
2015-11-05 17:16:13 +0300

    Made the script resistant to crashes. No need to recount already counted results.

--- model_matching/main_set_parallel.py
+++ model_matching/main_set_parallel.py
@@ -18,6 +18,7 @@ import math
 import os
 
 num_processes = 4
+apps_per_run = 200
 
 def analyze_malicious(package_list, similarities_found, similarities_found_by_perms, similarities_found_by_chains):
 	count_apps = 0
@@ -42,15 +43,38 @@ def analyze_malicious(package_list, similarities_found, similarities_found_by_pe
 		except:
 			continue
 
-if __name__ == "__main__":
-#Malicious part
+def process_list_apps(list_apps_f, similarities_found_by_perms_f, similarities_found_f, similarities_found_by_chains_f, tag):
+	#Already counted part
+	try:
+		f = open(similarities_found_by_perms_f, 'r')
+		counted_similarities_found_by_perms = json.loads(f.read())
+		f.close()
+	except:
+		counted_similarities_found_by_perms = {}
+
+	try:
+		f = open(similarities_found_f, 'r')
+		counted_similarities_found = json.loads(f.read())
+		f.close()
+	except:
+		counted_similarities_found = {}
+
+	try:
+		f = open(similarities_found_by_chains_f, 'r')
+		counted_similarities_found_by_chains = json.loads(f.read())
+		f.close()
+	except:
+		counted_similarities_found_by_chains = {}
+
 	package_list = []
-	analyzed_apps = 'malware_for_analysis.txt'
+	analyzed_apps = list_apps_f
 	f = open(analyzed_apps, 'r')
 
 	for line in f:
 		package_name = line[:-1]
-		package_list.append(package_name)
+		if not package_name in counted_similarities_found:
+			package_list.append(package_name)
+	print len(package_list), tag, 'samples to analyze'
 
 	f.close()
 
@@ -58,79 +82,50 @@ if __name__ == "__main__":
 	similarities_found = manager.dict()
 	similarities_found_by_perms = manager.dict()
 	similarities_found_by_chains = manager.dict()
-	processes = []
-	for i in range(num_processes):
-		list_analysis = manager.list(package_list[int(round(len(package_list) * i / num_processes, 0)) : int(round(len(package_list) * (i + 1) / num_processes, 0))])
-		p = Process(target=analyze_malicious, args=(list_analysis, similarities_found, \
-			similarities_found_by_perms, similarities_found_by_chains))
-		processes.append(p)
-		p.start()
-	for p in processes:
-		p.join()
 
-	f = open('similarities_found_by_perms_m.json', 'w')
-	simple_dict = dict(similarities_found_by_perms)
-	f.write(json.dumps(simple_dict, indent=4, separators=(',', ': ')))
-	f.close()
+	j = 0
+	while j * apps_per_run < len(package_list):
+		similarities_found = manager.dict()
+		similarities_found_by_perms = manager.dict()
+		similarities_found_by_chains = manager.dict()
 
-	f = open('similarities_found_m.json', 'w')
-	simple_dict = dict(similarities_found)
-	f.write(json.dumps(simple_dict, indent=4, separators=(',', ': ')))
-	f.close()
+		processes = []
+		for i in range(num_processes):
+			list_analysis = manager.list(package_list[(j + i) * apps_per_run : (j + i + 1) * apps_per_run if (j + i + 1) * apps_per_run < len(package_list) else len(package_list)])
+			p = Process(target=analyze_malicious, args=(list_analysis, similarities_found, \
+				similarities_found_by_perms, similarities_found_by_chains))
+			processes.append(p)
+			p.start()
+			if (j + i + 1) * apps_per_run >= len(package_list):
+				break
+		for p in processes:
+			p.join()
+		counted_similarities_found_by_perms.update(dict(similarities_found_by_perms))
+		counted_similarities_found.update(dict(similarities_found))
+		counted_similarities_found_by_chains.update(dict(similarities_found_by_chains))
 
-	f = open('similarities_found_by_chains_m.json', 'w')
-	simple_dict = dict(similarities_found_by_chains)
-	f.write(json.dumps(simple_dict, indent=4, separators=(',', ': ')))
-	f.close()
+		f = open(similarities_found_by_perms_f, 'w')
+		f.write(json.dumps(counted_similarities_found_by_perms, indent=4, separators=(',', ': ')))
+		f.close()
+
+		f = open(similarities_found_f, 'w')
+		f.write(json.dumps(counted_similarities_found, indent=4, separators=(',', ': ')))
+		f.close()
+
+		f = open(similarities_found_by_chains_f, 'w')
+		f.write(json.dumps(counted_similarities_found_by_chains, indent=4, separators=(',', ': ')))
+		f.close()
+		j += num_processes
 
 	count_m = 0
-	for package_name in simple_dict:
-		if simple_dict[package_name] != []:
+	for package_name in counted_similarities_found_by_chains:
+		if counted_similarities_found_by_chains[package_name] != []:
 			count_m += 1
-	print 'Identified as malicious in malicious tests', count_m, '/', len(simple_dict.keys())
+	print 'Identified as malicious in', tag, 'tests', count_m, '/', len(counted_similarities_found_by_chains.keys())
 
+if __name__ == "__main__":
+#Malicious part
+	process_list_apps('malware_for_analysis.txt', 'similarities_found_by_perms_m.json', 'similarities_found_m.json', 'similarities_found_by_chains_m.json', 'malicious')
 
 #Benign part
-	package_list = []
-	analyzed_apps = 'benign_for_analysis.txt'
-	f = open(analyzed_apps, 'r')
-
-	for line in f:
-		package_name = line[:-1]
-		package_list.append(package_name)
-
-	f.close()
-
-	similarities_found = manager.dict()
-	similarities_found_by_perms = manager.dict()
-	similarities_found_by_chains = manager.dict()
-	processes = []
-	for i in range(num_processes):
-		list_analysis = manager.list(package_list[int(round(len(package_list) * i / num_processes, 0)) : int(round(len(package_list) * (i + 1) / num_processes, 0))])
-		p = Process(target=analyze_malicious, args=(list_analysis, similarities_found, \
-			similarities_found_by_perms, similarities_found_by_chains))
-		processes.append(p)
-		p.start()
-	for p in processes:
-		p.join()
-
-	f = open('similarities_found_by_perms_b.json', 'w')
-	simple_dict = dict(similarities_found_by_perms)
-	f.write(json.dumps(simple_dict, indent=4, separators=(',', ': ')))
-	f.close()
-
-	f = open('similarities_found_b.json', 'w')
-	simple_dict = dict(similarities_found)
-	f.write(json.dumps(simple_dict, indent=4, separators=(',', ': ')))
-	f.close()
-
-	f = open('similarities_found_by_chains_b.json', 'w')
-	simple_dict = dict(similarities_found_by_chains)
-	f.write(json.dumps(simple_dict, indent=4, separators=(',', ': ')))
-	f.close()
-
-	count_m = 0
-	for package_name in simple_dict:
-		if simple_dict[package_name] != []:
-			count_m += 1
-	print 'Identified as malicious in benign tests', count_m, '/', len(simple_dict.keys())
+	process_list_apps('benign_for_analysis.txt', 'similarities_found_by_perms_b.json', 'similarities_found_b.json', 'similarities_found_by_chains_b.json', 'benign')
